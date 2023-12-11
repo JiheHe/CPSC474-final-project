7f55eb8806d9bf817044dcd20518a2383798fdb5
JiheHe <nickhe2003@gmail.com>
2023-12-10 21:47:18 -0500

    quick change

--- rummy.py
+++ rummy.py
@@ -1,32 +1,6 @@
-'''
-  Outline for 2 player Rummy
-  https://bicyclecards.com/how-to-play/rummy-rum
-  Ranking: K (highest) to A (lowest)
+'''Implements the rummy card game outlined in README'''
 
-  Setup:
-    One deck of cards, standard
-    Each player is dealt 10 cards from the deck one at a time face-down without replacements
-    The remaining cards are placed face down on the table, forming the stock.
-    The top card of the stock is turned face up and placed next to the stock to start the discard pile。
+from deck import Deck, Card
 
-  Play:
-    *Each player tries to form matched sets consisting of groups of three or four of a kind, or sequences of three or more cards of the same suit.
-    Dealer alternates, and non-dealer starts the round.
-    Each round, 
-      First, the player in-turn either draws the top card of the stock or takes the top card of the discard pile to add to his hand
-      Then, optionally (and):
-        the player may lay down as many meld (matched set) face up on the table as they have without consuming the turn.
-        the player may add one or more cards from their hand to any matched set already shown on the table.
-      Next, the player discards one card, face up, onto the discard pile.
-        *If the player has drawn from the discard pile, he may not discard the same card on that turn.
-    When a player gets rid of all of their cards, they win the game.
-      *If the player can get rid of all cards in the "optional" phase, then the round ends and no further discard or play is needed.
-    If the last card of the stock has been drawn and no player has gone out, the next player in turn may either take the top of the discard pile, or may turn the discard pile over to form a new stock (without shuffling it) and draw the top card. Play then proceeds as before.
-    
-  End Round:
-    Each player pays to the winner the pip value of the cards remaining in their hand, whether the cards form matched sets or not. Face cards count 10 each, aces 1 each, and every other card its pip value.
-      *Here we say the winner gains that many points
-    A player goes "rummy" when they get rid of all cards in their hand at once, without previously having put down or laid off any cards (i.e. no optional). In this event, every other player pays double - twice what opponents would otherwise owe.
-
-  Going to play N rounds. And the player with the most points in the end wins. 
-'''
+class Game:
+  pass
